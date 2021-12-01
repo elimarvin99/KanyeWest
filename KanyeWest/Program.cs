fe4b41a4cadf149d4931d137d8e33f29da7a6f2b
elimarvin99 <elimarvin99@gmail.com>
2021-12-01 10:56:21 -0500

    a bit of refactoring

--- KanyeWest/Program.cs
+++ KanyeWest/Program.cs
@@ -3,19 +3,21 @@ using System;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace KanyeWest
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            //var kanyeQuotes = new List<string>();
-            //var swansonQuotes = new List<string>();
+            var kanyeQuotes = new List<string>();
+            var swansonQuotes = new List<string>();
             Console.WriteLine("How many quotes do you want to get?");
             var input = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < input; i++)
+            for (int i = 1; i <= input; i++)
             {
+                Console.WriteLine($"Conversation #{i}");
                 var kanyeURL = "https://api.kanye.rest";
                 var kanyeClient = new HttpClient();
                 //send a get request to the url and get the json as a string and the result returns it to us as a string
@@ -31,9 +33,38 @@ namespace KanyeWest
                 var ronQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
                 //swansonQuotes.Add(ronQuote);
                 Console.WriteLine($"Swanson replies: {ronQuote}");
+                Console.WriteLine("-------------------------------------------");
                 Thread.Sleep(1000);
             }
-            
+
+            ////Donald Trump bonus section
+            //var trumpURL = "https://www.io.tronalddump.api";
+            //var trumpClient = new HttpClient();
+            //var trumpResponse = trumpClient.GetStringAsync(trumpURL).Result;
+            ////var trumpQuote = JObject.Parse(trumpResponse).GetValue("quote").ToString();
+            //Console.WriteLine($"Trumpy says: {trumpResponse}");
+
+            //COD section
+            //var client = new HttpClient();
+            //var request = new HttpRequestMessage
+            //{
+            //    Method = HttpMethod.Get,
+            //    RequestUri = new Uri("https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/Amartin743/psn"),
+            //    Headers =
+            //    {
+            //        { "x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com" },
+            //        { "x-rapidapi-key", "eecd9e3415msh53b4b079f89a9ccp126a2bjsn8aac4d755253" },
+            //    },
+            //};
+            //using (var response = await client.SendAsync(request))
+            //{
+            //    response.EnsureSuccessStatusCode();
+            //    var body = response.Content.ReadAsStringAsync().Result;
+            //    var quote = JObject.Parse(body).ToString();
+            //    Console.WriteLine(quote);
+            //}
+
+
 
         }
     }
